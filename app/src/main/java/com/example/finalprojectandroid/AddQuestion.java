3f83a458637e36e2819e20f7ce1107cc5c31844b
Alice Khodorkovsky <alish1999@gmail.com>
2023-02-14 15:49:34 +0200

    not finished

---
+++ app/src/main/java/com/example/finalprojectandroid/AddQuestion.java
@@ -0,0 +1,144 @@
+package com.example.finalprojectandroid;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.example.finalprojectandroid.R;
+import com.example.finalprojectandroid.Register;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+
+
+import java.io.IOException;
+import java.util.UUID;
+
+public class AddQuestion extends Fragment {
+    FirebaseStorage storage = FirebaseStorage.getInstance();
+    StorageReference storageReference = storage.getReferenceFromUrl("gs://finalprojectandroind.appspot.com/");
+    Boolean picChanged = false;
+    Uri filePath;
+    ImageView pProfileImage;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.add_question, container, false);
+        pProfileImage         = view.findViewById(R.id.profilePicReg);
+        EditText correctOption          = view.findViewById(R.id.correctOption);
+        EditText option2                = view.findViewById(R.id.option2);
+        EditText option3                = view.findViewById(R.id.option3);
+        EditText option4                = view.findViewById(R.id.option4);
+        EditText country                = view.findViewById(R.id.country);
+        EditText city                   = view.findViewById(R.id.city);
+        FloatingActionButton addImage   = view.findViewById(R.id.addProfilePic);
+        Button uploadButton             = view.findViewById(R.id.insertButton);
+
+        return view;
+    }
+    private void choosePic() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        mStartForResult.launch(intent);
+    }
+    ActivityResultLauncher<Intent> mStartForResult = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    if (result.getResultCode() == Activity.RESULT_OK &&
+                            result.getData() != null) {
+                        picChanged = true;
+                        Intent intent = result.getData();
+                        filePath = intent.getData();
+                        try{
+                            Bitmap bitmap = MediaStore.
+                                    Images.
+                                    Media.
+                                    getBitmap(getActivity().getContentResolver(),
+                                            filePath);
+                            pProfileImage.setImageBitmap(bitmap);
+                        }
+                        catch (IOException err){
+                            err.printStackTrace();
+                        }
+                    }
+                }
+            });
+    private String uploadImage(){
+        String imgName = UUID.randomUUID().toString();
+        StorageReference ref =
+                storageReference.child(
+                        "images/" + imgName);
+
+        ref.putFile(filePath)
+                .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        Log.d("ok", "Working");
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.e("not ok","Not working");
+                    }
+                });
+        return imgName;
+    }
+    }
