ec9236723219d7ff8e08d93af8f0dc779c459a4d
Daniel Akulich <daniakuli2@gmail.com>
2023-03-18 12:22:10 +0200

    Added Navigation graph and local database

---
+++ app/src/main/java/com/example/finalprojectandroid/Models/AppDatabase.java
@@ -0,0 +1,28 @@
+package com.example.finalprojectandroid.Models;
+
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+import com.example.finalprojectandroid.Interfaces.PicturesDao;
+
+@Database(entities = {Pictures.class}, version = 1)
+public abstract class AppDatabase extends RoomDatabase {
+
+    private static AppDatabase instance;
+
+    public abstract PicturesDao picturesDao();
+
+    public static synchronized AppDatabase getInstance(Context context){
+        if(instance == null){
+            instance = Room.databaseBuilder(context.getApplicationContext(),
+                                            AppDatabase.class,
+                                            "app_database")
+                            .fallbackToDestructiveMigration()
+                            .build();
+        }
+        return instance;
+    }
+}
